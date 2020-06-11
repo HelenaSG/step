8f6cb1ebfe97a705475964780648251229558a82
Helena Shangguan <helloprincesssg@gmail.com>
2020-06-11 15:57:28 +0000

    Add a pie chart.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
 /**
  * Adds a random fact to the page.
  */
@@ -128,4 +131,32 @@ function login() {
     const statsListElement = document.getElementById('login-status-container');
     statsListElement.innerHTML = authResponse.htmlContent;
   });
+}
+
+/** Creates a chart and adds it to the page. */
+function drawChart() {
+  const data = new google.visualization.DataTable();
+  data.addColumn('string', 'Hours per Day');
+  data.addColumn('number', 'Count');
+        data.addRows([
+          ['Coding',     11],
+          ['Cook',      2],
+          ['Eat',      1],
+          ['Entertain', 2],
+          ['Sleep',    8]
+        ]);
+
+  const options = {
+    'title': 'My Daily Activities',
+    'width': 500,
+    'height': 400,
+    'colors': ['#ef8f80', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
+    'backgroundColor': 'transparent',
+    'legend': {textStyle: {color: '#ffffff'}},
+    'titleTextStyle': {color: '#ffffff'},
+    'is3D': true
+  };
+
+  const chart = new google.visualization.PieChart(document.getElementById('chart-container'));
+  chart.draw(data, options);
 }
