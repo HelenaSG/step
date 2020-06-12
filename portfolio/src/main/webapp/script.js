86337937d3a5f7e77ab90040072bce58e9960f42
Helena Shangguan <helloprincesssg@gmail.com>
2020-06-12 16:31:10 +0000

    Update html for chart and map.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -132,31 +132,3 @@ function login() {
     statsListElement.innerHTML = authResponse.htmlContent;
   });
 }
-
-/** Create a chart and adds it to the page. */
-function drawChart() {
-  const data = new google.visualization.DataTable();
-  data.addColumn('string', 'Hours per Day');
-  data.addColumn('number', 'Count');
-        data.addRows([
-          ['Coding',     11],
-          ['Cook',      2],
-          ['Eat',      1],
-          ['Entertain', 2],
-          ['Sleep',    8]
-        ]);
-
-  const options = {
-    'title': 'My Daily Activities',
-    'width': 500,
-    'height': 400,
-    'colors': ['#ef8f80', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
-    'backgroundColor': 'transparent',
-    'legend': {textStyle: {color: '#ffffff'}},
-    'titleTextStyle': {color: '#ffffff'},
-    'is3D': true
-  };
-
-  const chart = new google.visualization.PieChart(document.getElementById('chart-container'));
-  chart.draw(data, options);
-}
